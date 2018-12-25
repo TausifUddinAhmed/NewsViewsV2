07c738c69bee5a06f200a0825be57ea0fef5f163
tausif11235 <tausif@localhost.localdomain>
2018-12-26 01:36:57 +0600

    Add_Navigation_Drawer

--- NewsViews/app/src/main/java/com/example/tausif/newsviews/ui/MainActivity.java
+++ NewsViews/app/src/main/java/com/example/tausif/newsviews/ui/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.tausif.newsviews.ui;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import com.example.tausif.newsviews.R;
 import com.example.tausif.newsviews.model.NewsApiResponse;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity implements MainViewInterface {
 
 
     MainPresenter mainPresenter;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
 
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
@@ -26,8 +39,56 @@ public class MainActivity extends AppCompatActivity implements MainViewInterface
         ButterKnife.bind(this);
         setupMVP();
         getNewsList();
+        setupNavigationDrawer();
+
+
+        navigationView = (NavigationView)findViewById(R.id.nv);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home",Toast.LENGTH_SHORT).show();
+                    case R.id.about:
+                        Toast.makeText(MainActivity.this, "About",Toast.LENGTH_SHORT).show();
+                    case R.id.exit:
+                        Toast.makeText(MainActivity.this, "Exit",Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+
+
+
+
+            }
+        });
+
+
+
     }
 
+
+    private void setupNavigationDrawer(){
+
+        drawerLayout= (DrawerLayout)findViewById(R.id.activity_main);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
 
     private void setupMVP() {
         mainPresenter = new MainPresenter(this);
@@ -36,21 +97,6 @@ public class MainActivity extends AppCompatActivity implements MainViewInterface
     private void getNewsList() {
 
         mainPresenter.getNews();
-
-    }
-
-    private void GetNew() {
-
-
-//        //Progress dialog initialization so that users doesn't get bored :P
-//        progressDialog = new ProgressDialog(MainActivity.this);
-//        progressDialog.setMessage("Loading..."); // Setting Message
-//        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER); // Progress Dialog Style Spinner
-//        progressDialog.show(); // Display Progress Dialog
-//        progressDialog.setCancelable(false);
-
-        //controller class initialization and called the method of class
-
 
     }
 
