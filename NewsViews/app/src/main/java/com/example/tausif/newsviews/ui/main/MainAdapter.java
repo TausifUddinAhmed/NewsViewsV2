af4460c1f9642d536284a68f4f73abe85f79ff25
tausif11235 <tausif@localhost.localdomain>
2018-12-29 22:31:50 +0600

    Add_Tap_On_Card

--- NewsViews/app/src/main/java/com/example/tausif/newsviews/ui/main/MainAdapter.java
+++ NewsViews/app/src/main/java/com/example/tausif/newsviews/ui/main/MainAdapter.java
@@ -1,7 +1,12 @@
 package com.example.tausif.newsviews.ui.main;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +18,11 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.tausif.newsviews.R;
 import com.example.tausif.newsviews.model.news.Article;
+import com.example.tausif.newsviews.ui.webview.WebViewActivity;
+
 import java.util.List;
+
+import static android.content.Intent.CATEGORY_BROWSABLE;
 
 public class MainAdapter extends  RecyclerView.Adapter<MainAdapter.NewsViewHolder> {
 
@@ -91,10 +100,49 @@ public class MainAdapter extends  RecyclerView.Adapter<MainAdapter.NewsViewHolde
                  public void onClick(View v) {
                      int position = getAdapterPosition();
                      Toast.makeText(context, articles.get(position).getAuthor(), Toast.LENGTH_SHORT).show();
+
+                     final String url = articles.get(position).getUrl();
+
+                    // OpenAlertDilog();
+
+                    // Resources.getSystem().getString(R.string.load_content);
+                     AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                     builder.setTitle("NewsViews");
+                     builder.setMessage("Load the content in your mobile browser ?");
+                     builder.setPositiveButton("YES",
+                             new DialogInterface.OnClickListener() {
+                              public void onClick(DialogInterface dialog, int which) {
+
+
+                                    Toast.makeText(context, "OK was clicked", Toast.LENGTH_SHORT).show();
+                                     Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                                     browserIntent.addCategory(CATEGORY_BROWSABLE);
+                                     context.startActivity(browserIntent);
+
+
+                                 }
+                             });
+                     builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                         public void onClick(DialogInterface dialog, int which) {
+
+                             Intent intent = new Intent(context, WebViewActivity.class);
+                             intent.putExtra("URL", url);
+                             context.startActivity(intent);
+
+                             Toast.makeText(context, android.R.string.no, Toast.LENGTH_SHORT).show();
+                         }
+                     });
+
+
+                     builder.setCancelable(false);
+                     builder.show();
                  }
              });
 
 
         }
-    }
+
+
+
+        }
 }
